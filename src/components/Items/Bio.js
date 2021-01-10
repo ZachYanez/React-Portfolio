a403229fbdee0f5a63b929911bfe0d02ed9a2485
Zach Yanez <zachyanez@gmail.com>
2021-01-10 14:56:02 -0600

    Fixing graduation space

--- src/components/Items/Bio.js
+++ src/components/Items/Bio.js
@@ -5,7 +5,7 @@ export default function Bio() {
         <div  className="textColorWhite bioContent ml-5">
             Zachary Yanez is a Musician, Model, Web Developer, Teacher, and published author. Graduating from DePaul University School of Music in 2015, Zach has spent the last several years in Austin, Texas pursuing an exciting music career and picking up as many skills as he could along the way.<div/>
             <br/>
-            In late 2017 he signed on to The Brown Agency where he continues to work as a commercial print model & actor. He has appeared in ads for companies such as Amazon, Apple, Dell, LegalZoom, Bayer, H-E-B, Kevita, Ascension, and ConocoPhilips among others. <div/>
+            In late 2017 he signed on to The Brown Agency where he continues to work as a commercial print model and actor. He has appeared in ads for companies such as Amazon, Apple, Dell, LegalZoom, Bayer, H-E-B, Kevita, Ascension, and ConocoPhilips among others. <div/>
             <br/>
             As a teacher, Zach began working with students of all ages and abilities as soon as he returned from DePaul. He currently teaches piano lessons to students ages 6-12 for Westlake Lessons, and has worked for the non-profits All Rhythms and Beyond The Grade teaching percussion and piano classes to underserved communities in the Austin area.
             <div/>
